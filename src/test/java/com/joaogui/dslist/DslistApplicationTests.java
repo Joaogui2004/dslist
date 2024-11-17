fab558d1b09c809335af5a00610a11147ef4e478
João Guilherme Pontes Ramos <joaoguiofficial2004@gmail.noreply.github.com>
2024-11-16 22:08:56 -0300

    Project created

---
+++ src/test/java/com/joaogui/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaogui.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
